2fd77aa57d25b3d82f8723f74cc7c2f1a037a937
lennczar <lennczar@gmail.com>
2024-02-01 23:10:36 +0100

    update vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/UMNFT-Website/',
-  plugins: [react()],
-})
+	base: "https://ErichDomme.github.io/UMNFT-Website/",
+	plugins: [react()],
+});
