75547b0d4be24fe163ae64fd4d5ba6163f03085a
lennczar <lennczar@gmail.com>
2024-02-02 01:15:27 +0100

    test

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,6 +1,7 @@
 const PINATA_GATEWAY = "amaranth-purring-primate-230.mypinata.cloud";
 
 export async function readFromIPFS(cid: string): Promise<Uint8Array> {
+	console.log(`https://${PINATA_GATEWAY}/ipfs/${cid}`, fetch);
 	return await fetch(`https://${PINATA_GATEWAY}/ipfs/${cid}`)
 		.then((res) => res.arrayBuffer())
 		.then((res) => new Uint8Array(res));
