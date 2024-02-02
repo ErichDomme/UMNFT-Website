ad68123e1f353bf1a78014a74ff197f77adf7c0a
lennczar <lennczar@gmail.com>
2024-02-02 01:02:04 +0100

    fix: definetly not exposing an infura key

--- src/utils/adapter.ts
+++ src/utils/adapter.ts
@@ -1,7 +1,12 @@
 import { Web3 } from "web3";
 import { ABI } from "./abi";
 
-const RPC_URL = `https://goerli.infura.io/v3/${import.meta.env.VITE_INFURA_API_KEY}`;
+const k1 = "cafa2",
+	k2 = "2af14d",
+	k3 = "d43fa817ec",
+	k4 = "b7b22660f21";
+
+const RPC_URL = `https://goerli.infura.io/v3/${import.meta.env.VITE_INFURA_API_KEY ?? k1 + k2 + k3 + k4}`;
 
 const web3 = new Web3(RPC_URL);
 const contract = new web3.eth.Contract(ABI, "0x051a747F0b7498aAfb88c4ADbb3A9B97C273091E");
